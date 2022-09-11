846261ce0be73c901f41f8e93ed93d574478ddc8
Vlad Filonenko <filonenkovlad5@gmail.com>
2022-09-11 19:40:59 +0300

    1

---
+++ LR4/script.js
@@ -0,0 +1,36 @@
+// Завдання 1 --------------------------------
+function randomColor(){
+    max = 255
+    min = 0
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function bcEx1(){
+    document.getElementById("ex1").style.backgroundColor = "rgb("+randomColor()+","+randomColor()+","+randomColor()+")"
+    document.getElementById("ex1").style.color = "rgb("+randomColor()+","+randomColor()+","+randomColor()+")"
+}
+
+function bcEx2(){
+    document.querySelector("#ex2").style.backgroundColor = "rgb("+randomColor()+","+randomColor()+","+randomColor()+")"
+    document.querySelector("#ex2").style.color = "rgb("+randomColor()+","+randomColor()+","+randomColor()+")"
+}
+
+// Завдання 2 -------------------------------
+function add(){
+    document.getElementById("img").src = "images/Carpathian.jpg"
+    document.getElementById("img").style.display = ""        
+}
+
+var size = 500
+
+function increase(){
+    size += 100
+    document.getElementById("img").style.width = size + "px"
+}
+function reduce(){
+    size -= 100
+    document.getElementById("img").style.width = size + "px"   
+}
+function delet(){
+    document.getElementById("img").style.display = "none"    
+}
